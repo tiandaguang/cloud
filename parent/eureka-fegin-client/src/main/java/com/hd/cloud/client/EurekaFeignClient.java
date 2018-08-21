d75804dd218a8ab0d1839f76e1a21cf5e4b87379
Tiandaguang <zhizunwudivip2000@163.com>
2018-08-21 12:40:41 +0800

    Ribbon和Feign中使用熔断器

--- parent/eureka-fegin-client/src/main/java/com/hd/cloud/client/EurekaFeignClient.java
+++ parent/eureka-fegin-client/src/main/java/com/hd/cloud/client/EurekaFeignClient.java
@@ -1,14 +1,17 @@
 package com.hd.cloud.client;
 
 import com.hd.cloud.config.EurekaFeignConfig;
+import com.hd.cloud.service.EurekaHystrixConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Administrator on 2018/8/21.
+ *
+ * Feign客户端
  */
-@FeignClient(value = "eureka-client", configuration = EurekaFeignConfig.class)
+@FeignClient(value = "eureka-client", configuration = EurekaFeignConfig.class, fallback = EurekaHystrixConfig.class)
 public interface EurekaFeignClient {
 
     @GetMapping(value = "hi")
---
+++ parent/eureka-fegin-client/src/main/java/com/hd/cloud/service/EurekaHystrixConfig.java
@@ -0,0 +1,18 @@
+package com.hd.cloud.service;
+
+import com.hd.cloud.client.EurekaFeignClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2018/8/21.
+ * <p>
+ * 熔断器业务类
+ */
+@Component
+public class EurekaHystrixConfig implements EurekaFeignClient {
+
+    @Override
+    public String hi(String name) {
+        return "hi ," + name + ",sorry ,error!";
+    }
+}
