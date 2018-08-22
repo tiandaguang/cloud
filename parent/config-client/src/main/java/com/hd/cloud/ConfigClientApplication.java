0a7cbd9af68a1e32d0030bc9adcdeda8dca1be70
Tiandaguang <zhizunwudivip2000@163.com>
2018-08-22 15:45:26 +0800

    结合RabbitMQ使用消息总线bus更新远程配置文件

--- parent/config-client/src/main/java/com/hd/cloud/ConfigClientApplication.java
+++ parent/config-client/src/main/java/com/hd/cloud/ConfigClientApplication.java
@@ -2,14 +2,15 @@ package com.hd.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /***
  * 从本地读取配置文件
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
-
 }
--- parent/config-client/src/main/java/com/hd/cloud/controller/ConfigClientController.java
+++ parent/config-client/src/main/java/com/hd/cloud/controller/ConfigClientController.java
@@ -1,14 +1,15 @@
 package com.hd.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /***
  * 从本地读取配置文件
  */
-
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${foo}")
