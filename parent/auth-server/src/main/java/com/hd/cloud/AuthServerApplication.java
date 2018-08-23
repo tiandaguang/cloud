f19cad7e83292b5375912216e77805ddb7c52538
Tiandaguang <zhizunwudivip2000@163.com>
2018-08-23 20:48:28 +0800

    OAtu2.0验证

--- parent/auth-server/src/main/java/com/hd/cloud/AuthServerApplication.java
+++ parent/auth-server/src/main/java/com/hd/cloud/AuthServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
     @Bean
--- parent/auth-server2/src/main/java/com/hd/cloud/config/AuthorizationServerConfiguration.java
+++ parent/auth-server2/src/main/java/com/hd/cloud/config/AuthorizationServerConfiguration.java
@@ -8,15 +8,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableAuthorizationServer
@@ -47,37 +44,12 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private ClientDetailsService clientDetails;
 
-//    @Override
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.inMemory() // 使用in-memory存储
-//                .withClient("client") // client_id
-//                .secret("secret") // client_secret
-//                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
-//                .scopes("app"); // 允许的授权范围
-//    }
-
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(tokenStore());
-
-        // 配置TokenServices参数
-        DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenStore(endpoints.getTokenStore());
-        tokenServices.setSupportRefreshToken(false);
-        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
-        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
-        tokenServices.setAccessTokenValiditySeconds( (int) TimeUnit.DAYS.toSeconds(30)); // 30天
-        endpoints.tokenServices(tokenServices);
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory() // 使用in-memory存储
+                .withClient("client") // client_id
+                .secret("secret") // client_secret
+                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
+                .scopes("app"); // 允许的授权范围
     }
-
-//    @Bean
-//    @Primary
-//    public DefaultTokenServices tokenServices() {
-//        DefaultTokenServices tokenServices = new DefaultTokenServices();
-//        tokenServices.setSupportRefreshToken(true);
-//        tokenServices.setTokenStore(tokenStore);
-//        return tokenServices;
-//    }
-
 }
