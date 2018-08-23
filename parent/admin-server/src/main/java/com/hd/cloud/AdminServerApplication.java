5f058be6019ca4823c3c9b7f138aa7d36472b8a9
Tiandaguang <zhizunwudivip2000@163.com>
2018-08-23 13:44:47 +0800

    使用Admin监控

--- parent/admin-server/src/main/java/com/hd/cloud/AdminServerApplication.java
+++ parent/admin-server/src/main/java/com/hd/cloud/AdminServerApplication.java
@@ -1,14 +1,14 @@
 package com.hd.cloud;
 
-
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableAdminServer
+public class AdminServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(AdminServerApplication.class, args);
     }
 }
--- parent/config-client/src/main/java/com/hd/cloud/ConfigClientApplication.java
+++ parent/config-client/src/main/java/com/hd/cloud/ConfigClientApplication.java
@@ -1,10 +1,8 @@
 package com.hd.cloud;
 
-import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 /***
  * 从本地读取配置文件
@@ -13,10 +11,6 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 public class ConfigClientApplication {
 
-    @Bean
-    public Sampler defaultSampler() {
-        return Sampler.ALWAYS_SAMPLE;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
