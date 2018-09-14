fd595d6cd5ae96ca4601b8468e3692e2d84031df
Tian Daguang <zhizunwudivip2000.com>
2018-09-14 10:41:09 +0800

    增加eureka_server的acutor配置

---
+++ baike-scloud/eureka_server/src/main/java/com/hd/cloud/Mymd5.java
@@ -0,0 +1,34 @@
+package com.hd.cloud;
+
+import java.security.MessageDigest;
+
+public class Mymd5 {
+
+    public static final String getMD5(String s) {
+        char[] hexDigits = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        try {
+            byte[] e = s.getBytes("utf-8");
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(e);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char[] str = new char[j * 2];
+            int k = 0;
+
+            for(int i = 0; i < j; ++i) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 15];
+                str[k++] = hexDigits[byte0 & 15];
+            }
+
+            return new String(str);
+        } catch (Exception var10) {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Mymd5.getMD5("a1b2c3ipad12345"));
+    }
+}
