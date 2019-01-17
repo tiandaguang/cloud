1dc424e36f8d41c1eb93ab9e476255080ae96d54
tiandaguang <daguang.tian@ucarinc.com>
2019-01-17 18:52:27 +0800

    stream-kafka

---
+++ baike-scloud/eureka-stream-kafka-consumer/src/main/java/com/hd/cloud/KafkaConsumerApplication.java
@@ -0,0 +1,30 @@
+package com.hd.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KafkaConsumerApplication {
+
+    @Value("${server.port}")
+    String port;
+
+//    @Bean
+//    public Sampler defaultSampler() {
+//        return Sampler.ALWAYS_SAMPLE;
+//    }
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+    }
+}
