97a58a01c19583773daf48f3675a2813ea780f77
tiandaguang <daguang.tian@ucarinc.com>
2019-01-17 18:08:52 +0800

    修改报错

--- baike-scloud/auth-server-db/src/main/java/com/hd/cloud/config/WebSecurityConfiguration.java
+++ baike-scloud/auth-server-db/src/main/java/com/hd/cloud/config/WebSecurityConfiguration.java
@@ -4,10 +4,12 @@ import com.hd.cloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
